32b80278610dc3ec6e74509fecabb71648a9dbfa
kimtaehan <sungsoo1470@naver.com>
2025-05-17 23:37:59 +0900

    Add files via upload

---
+++ 56.py
@@ -0,0 +1,39 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/김현서/OneDrive/바탕 화면/11주차 과제.py ===============
+>>> 
+================ RESTART: C:/Users/김현서/OneDrive/바탕 화면/11주차 과제.py ===============
+>>> 
+================ RESTART: C:/Users/김현서/OneDrive/바탕 화면/11주차 과제.py ===============
+>>> (123).__add__(334)
+457
+>>> (123).__sub__(334)
+-211
+>>> (123).__mul__(334)
+41082
+>>> (123).__truediv__(334)
+0.36826347305389223
+>>> (123).__truediv__(3)
+41.0
+>>> 
+================ RESTART: C:/Users/김현서/OneDrive/바탕 화면/11주차 과제.py ===============
+1 : True
+2 : True
+3 : False
+4 : False
+5 : True
+6 : False
+7 : False
+8 : False
+9 : False
+10: True
+>>> 
+================ RESTART: C:/Users/김현서/OneDrive/바탕 화면/11주차 과제.py ===============
+>>> class Dog:
+...     def __init__(self, name, age):
+...         self.name = name
+...         self.age = age
+... 
+...         
+>>> 
